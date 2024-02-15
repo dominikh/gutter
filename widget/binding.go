ea52c50098e415f0260c2f8f062d81e4adb8ca08
Dominik Honnef <dominik@honnef.co>
2024-02-15 20:02:54 +0100

    widget: add ProxyElement

--- widget/binding.go
+++
@@ -1,6 +0,0 @@
-// SPDX-FileCopyrightText: 2014 The Flutter Authors. All rights reserved.
-// SPDX-FileCopyrightText: 2024 Dominik Honnef and contributors
-//
-// SPDX-License-Identifier: MIT AND BSD-3-Clause
-
-package widget
