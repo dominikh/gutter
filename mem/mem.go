dab3329a0ee46d7a2bd5f3ca31d7ad80406e81a2
Dominik Honnef <dominik@honnef.co>
2024-09-10 14:21:10 +0200

    mem: track generation of double buffered slices

--- mem/mem.go
+++ mem/mem.go
@@ -5,19 +5,23 @@
 package mem
 
 type DoubleBuffered[T any] struct {
+	Generation  uint32
 	Front, Back T
 }
 
 func (db *DoubleBuffered[T]) Swap() {
 	db.Front, db.Back = db.Back, db.Front
+	db.Generation++
 }
 
 type DoubleBufferedSlice[T any] struct {
+	Generation  uint32
 	Front, Back []T
 }
 
 func (db *DoubleBufferedSlice[T]) Swap() {
 	db.Front, db.Back = db.Back[:0], db.Front[:0]
+	db.Generation++
 }
 
 func CopyMap[K comparable, V any, M ~map[K]V](m M) M {
