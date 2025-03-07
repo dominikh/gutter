957b740ec6a9d29d0a209a7920a2a6d5091f6811
Dominik Honnef <dominik@honnef.co>
2025-03-07 03:27:39 +0100

    internal/sparse: add benchmarks for clip filling

--- internal/sparse/fine_test.go
+++ internal/sparse/fine_test.go
@@ -30,6 +30,29 @@ func benchmarkFill(b *testing.B, fn func(b *testing.B, buf [][stripHeight]Color)
 	}
 }
 
+func benchmarkClipFill(b *testing.B, fn func(b *testing.B, dst, src [][stripHeight]Color)) {
+	dst := make([][stripHeight]Color, wideTileWidth)
+	src := make([][stripHeight]Color, wideTileWidth)
+	// make sure memory is paged in
+	clear(dst)
+	clear(src)
+
+	// We test the full width to measure the best possible performance, and at
+	// the smallest possible width to measure the per-call overhead.
+	fillWidths := []int{wideTileWidth, 1}
+	for _, width := range fillWidths {
+		b.Run(fmt.Sprintf("width=%d", width), func(b *testing.B) {
+			fn(b, dst, src)
+			px := float64(width * tileHeight * b.N)
+			d := float64(b.Elapsed()) / px
+			bytes := px * 4 * 4
+			r := bytes / float64(b.Elapsed().Seconds())
+			b.ReportMetric(d, "ns/px")
+			b.ReportMetric(r, "B/s")
+		})
+	}
+}
+
 func Benchmark_fineFillSolidNative(b *testing.B) {
 	c := Color{1, 1, 1, 1}
 	benchmarkFill(b, func(b *testing.B, buf [][stripHeight]Color) {
@@ -54,6 +77,32 @@ func Benchmark_fineFillComplexNative(b *testing.B) {
 	benchmarkFill(b, func(b *testing.B, buf [][stripHeight]Color) {
 		for b.Loop() {
 			fineFillComplexNative(buf, c)
+		}
+	})
+}
+
+func Benchmark_fineClipFillSimpleNosNative(b *testing.B) {
+	nos := Color{1, 1, 1, 1}
+	benchmarkClipFill(b, func(b *testing.B, dst, src [][stripHeight]Color) {
+		for b.Loop() {
+			fineClipFillSimpleNosNative(dst, nos, src)
+		}
+	})
+}
+
+func Benchmark_fineClipFillSimpleTosTranslucentNative(b *testing.B) {
+	tos := Color{0.5, 0.5, 0.5, 0.5}
+	benchmarkClipFill(b, func(b *testing.B, dst, src [][stripHeight]Color) {
+		for b.Loop() {
+			fineClipFillSimpleTosTranslucentNative(dst, tos)
+		}
+	})
+}
+
+func Benchmark_fineClipFillNative(b *testing.B) {
+	benchmarkClipFill(b, func(b *testing.B, dst, src [][stripHeight]Color) {
+		for b.Loop() {
+			fineClipFillNative(dst, src)
 		}
 	})
 }
