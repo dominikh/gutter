488f5d57d54b9a0b53df6cc8215dacdf6fd20ea5
Dominik Honnef <dominik@honnef.co>
2025-03-14 05:12:09 +0100

    internal/sparse: add tests

--- internal/sparse/fine_test.go
+++ internal/sparse/fine_test.go
@@ -5,9 +5,505 @@
 package sparse
 
 import (
+	"encoding/binary"
+	"flag"
 	"fmt"
+	"image"
+	"image/png"
+	"os"
 	"testing"
+
+	"honnef.co/go/curve"
+	"honnef.co/go/safeish"
 )
+
+var writeGolden = flag.Bool("write-golden", false, "Write golden files")
+
+func getCtx(width, height uint16, transparent bool) *CsRenderCtx {
+	ctx := NewCsRenderCtx(width, height)
+	if !transparent {
+		ctx.Fill(
+			curve.NewRectFromOrigin(curve.Pt(0, 0), curve.Sz(float64(width), float64(height))).PathElements(0.1),
+			NonZero,
+			Color{1, 1, 1, 1},
+		)
+	}
+	return ctx
+}
+
+func renderAndCompare(t *testing.T, width, height uint16, transparent bool, name string, fn func(ctx *CsRenderCtx)) {
+	t.Helper()
+
+	ctx := getCtx(width, height, transparent)
+	fn(ctx)
+	compareRendered(t, ctx, name)
+}
+
+func render(ctx *CsRenderCtx) []Color {
+	pixmap := make([]Color, int(ctx.width)*int(ctx.height))
+	ctx.RenderToPixmap(ctx.width, ctx.height, pixmap)
+	return pixmap
+}
+
+func compareRendered(t *testing.T, ctx *CsRenderCtx, name string) {
+	t.Helper()
+
+	pixmap := render(ctx)
+	img := image.NewNRGBA64(image.Rect(0, 0, int(ctx.width), int(ctx.height)))
+	writeF32AsU16(pixmap, safeish.Cast[[][8]uint8](img.Pix))
+
+	if *writeGolden {
+		f, err := os.Create("./testdata/golden/" + name + ".png")
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer f.Close()
+		if err := png.Encode(f, img); err != nil {
+			t.Fatal(err)
+		}
+		return
+	}
+
+	f, err := os.Open("./testdata/golden/" + name + ".png")
+	if err != nil {
+		t.Fatal(err)
+	}
+	golden, err := png.Decode(f)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var failed bool
+	var worstDiff int64
+	var numWrong int
+	if golden.Bounds().Dx() != int(ctx.width) || golden.Bounds().Dy() != int(ctx.height) {
+		t.Errorf("got size (%d, %d), want (%d, %d)", ctx.width, ctx.height, golden.Bounds().Dx(), golden.Bounds().Dy())
+		failed = true
+	} else {
+		for x := range ctx.width {
+			for y := range ctx.height {
+				ir, ig, ib, ia := img.At(int(x), int(y)).RGBA()
+				gr, gg, gb, ga := golden.At(int(x), int(y)).RGBA()
+
+				abs := func(a int64) int64 {
+					if a >= 0 {
+						return a
+					} else {
+						return -a
+					}
+				}
+
+				const maxDiff = 257
+				dr := abs(int64(ir) - int64(gr))
+				dg := abs(int64(ig) - int64(gg))
+				db := abs(int64(ib) - int64(gb))
+				da := abs(int64(ia) - int64(ga))
+
+				if dr > maxDiff || dg > maxDiff || db > maxDiff || da > maxDiff {
+					worstDiff = max(dr, dg, db, da, worstDiff)
+					numWrong++
+					failed = true
+				}
+			}
+		}
+	}
+
+	if failed {
+		if err := os.MkdirAll("./testdata/failed", 0777); err != nil {
+			t.Fatal(err)
+		}
+		ff, err := os.Create("./testdata/failed/" + name + ".png")
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer ff.Close()
+		if err := png.Encode(ff, img); err != nil {
+			t.Fatal(err)
+		}
+
+		if worstDiff > 0 {
+			t.Errorf("%d pixels were wrong, by up to %d/65535", numWrong, worstDiff)
+		}
+		t.Fatalf("result (./testdata/failed/%[1]s.png) doesn't match golden file (./testdata/golden/%[1]s.png)", name)
+	}
+}
+
+func TestIncorrectFilling1(t *testing.T) {
+	// https://github.com/LaurenzV/cpu-sparse-experiments/issues/2
+	renderAndCompare(t, 8, 8, false, "incorrect_filling_1", func(ctx *CsRenderCtx) {
+		var path curve.BezPath
+		path.MoveTo(curve.Pt(4, 0))
+		path.LineTo(curve.Pt(8, 4))
+		path.LineTo(curve.Pt(4, 8))
+		path.LineTo(curve.Pt(0, 4))
+		path.ClosePath()
+
+		ctx.Fill(path.Elements(), NonZero, Color{0, 1, 0, 1})
+	})
+}
+
+func TestIncorrectFilling2(t *testing.T) {
+	// https://github.com/LaurenzV/cpu-sparse-experiments/issues/2
+	renderAndCompare(t, 64, 64, false, "incorrect_filling_2", func(ctx *CsRenderCtx) {
+		var path curve.BezPath
+		path.MoveTo(curve.Pt(16, 16))
+		path.LineTo(curve.Pt(48, 16))
+		path.LineTo(curve.Pt(48, 48))
+		path.LineTo(curve.Pt(16, 48))
+		path.ClosePath()
+
+		ctx.Fill(path.Elements(), NonZero, Color{0, 1, 0, 1})
+	})
+}
+
+func TestIncorrectFilling3(t *testing.T) {
+	// https://github.com/LaurenzV/cpu-sparse-experiments/issues/2
+	renderAndCompare(t, 9, 9, false, "incorrect_filling_3", func(ctx *CsRenderCtx) {
+		var path curve.BezPath
+		path.MoveTo(curve.Pt(4.00001, 1e-45))
+		path.LineTo(curve.Pt(8.00001, 4.00001))
+		path.LineTo(curve.Pt(4.00001, 8.00001))
+		path.LineTo(curve.Pt(1e-45, 4.00001))
+		path.ClosePath()
+
+		ctx.Fill(path.Elements(), NonZero, Color{0, 1, 0, 1})
+	})
+}
+
+func TestIncorrectFilling4(t *testing.T) {
+	// https://github.com/LaurenzV/cpu-sparse-experiments/issues/2
+	renderAndCompare(t, 64, 64, false, "incorrect_filling_4", func(ctx *CsRenderCtx) {
+		var path curve.BezPath
+		path.MoveTo(curve.Pt(16.000002, 8))
+		path.LineTo(curve.Pt(20.000002, 8))
+		path.LineTo(curve.Pt(24.000002, 8))
+		path.LineTo(curve.Pt(28.000002, 8))
+		path.LineTo(curve.Pt(32.000002, 8))
+		path.LineTo(curve.Pt(32.000002, 9))
+		path.LineTo(curve.Pt(28.000002, 9))
+		path.LineTo(curve.Pt(24.000002, 9))
+		path.LineTo(curve.Pt(20.000002, 9))
+		path.LineTo(curve.Pt(16.000002, 9))
+		path.ClosePath()
+
+		ctx.Fill(path.Elements(), NonZero, Color{0, 1, 0, 1})
+	})
+}
+
+func TestIncorrectFilling5(t *testing.T) {
+	// https://github.com/LaurenzV/cpu-sparse-experiments/issues/2
+	renderAndCompare(t, 32, 32, false, "incorrect_filling_5", func(ctx *CsRenderCtx) {
+		var path curve.BezPath
+		path.MoveTo(curve.Pt(16, 8))
+		path.LineTo(curve.Pt(16, 9))
+		path.LineTo(curve.Pt(32, 9))
+		path.LineTo(curve.Pt(32, 8))
+		path.ClosePath()
+
+		ctx.Fill(path.Elements(), NonZero, Color{0, 1, 0, 1})
+	})
+}
+
+func TestIncorrectFilling6(t *testing.T) {
+	// https://github.com/LaurenzV/cpu-sparse-experiments/issues/2
+	renderAndCompare(t, 32, 32, false, "incorrect_filling_6", func(ctx *CsRenderCtx) {
+		var path curve.BezPath
+		path.MoveTo(curve.Pt(16, 8))
+		path.LineTo(curve.Pt(31.999998, 8))
+		path.LineTo(curve.Pt(31.999998, 9))
+		path.LineTo(curve.Pt(16, 9))
+		path.ClosePath()
+
+		ctx.Fill(path.Elements(), NonZero, Color{0, 1, 0, 1})
+	})
+}
+
+func TestIncorrectFilling7(t *testing.T) {
+	// https://github.com/LaurenzV/cpu-sparse-experiments/issues/2
+	renderAndCompare(t, 32, 32, false, "incorrect_filling_7", func(ctx *CsRenderCtx) {
+		var path curve.BezPath
+		path.MoveTo(curve.Pt(32.000002, 9))
+		path.LineTo(curve.Pt(28, 9))
+		path.LineTo(curve.Pt(28, 8))
+		path.LineTo(curve.Pt(32.000002, 8))
+		path.ClosePath()
+
+		ctx.Fill(path.Elements(), NonZero, Color{0, 1, 0, 1})
+	})
+}
+
+func TestIncorrectFilling8(t *testing.T) {
+	// https://github.com/LaurenzV/cpu-sparse-experiments/issues/2
+	renderAndCompare(t, 32, 32, false, "incorrect_filling_8", func(ctx *CsRenderCtx) {
+		var path curve.BezPath
+		path.MoveTo(curve.Pt(16.000427, 8))
+		path.LineTo(curve.Pt(20.000427, 8))
+		path.LineTo(curve.Pt(24.000427, 8))
+		path.LineTo(curve.Pt(28.000427, 8))
+		path.LineTo(curve.Pt(32.000427, 8))
+		path.LineTo(curve.Pt(32.000427, 9))
+		path.LineTo(curve.Pt(28.000427, 9))
+		path.LineTo(curve.Pt(24.000427, 9))
+		path.LineTo(curve.Pt(20.000427, 9))
+		path.LineTo(curve.Pt(16.000427, 9))
+		path.ClosePath()
+
+		ctx.Fill(path.Elements(), NonZero, Color{0, 1, 0, 1})
+	})
+}
+
+func TestOutOfBoundStrip(t *testing.T) {
+	// https://github.com/LaurenzV/cpu-sparse-experiments/issues/11
+	ctx := getCtx(256, 256, true)
+	var path curve.BezPath
+	path.MoveTo(curve.Pt(256, 254))
+	path.LineTo(curve.Pt(265, 254))
+	ctx.Stroke(path.Elements(), curve.DefaultStroke.WithWidth(1), Color{1, 0, 0, 1})
+	// Test that we don't panic.
+	render(ctx)
+}
+
+func starPath() curve.BezPath {
+	var path curve.BezPath
+	path.MoveTo(curve.Pt(50.0, 10.0))
+	path.LineTo(curve.Pt(75.0, 90.0))
+	path.LineTo(curve.Pt(10.0, 40.0))
+	path.LineTo(curve.Pt(90.0, 40.0))
+	path.LineTo(curve.Pt(25.0, 90.0))
+	path.LineTo(curve.Pt(50.0, 10.0))
+	return path
+}
+
+func TestFillingNonZeroRule(t *testing.T) {
+	renderAndCompare(t, 100, 100, false, "filling_nonzero_rule", func(ctx *CsRenderCtx) {
+		star := starPath()
+		ctx.Fill(star.Elements(), NonZero, Color{0.5, 0, 0, 1})
+	})
+}
+
+func TestFillingEvenOddRule(t *testing.T) {
+	renderAndCompare(t, 100, 100, false, "filling_evenodd_rule", func(ctx *CsRenderCtx) {
+		star := starPath()
+		ctx.Fill(star.Elements(), EvenOdd, Color{0.5, 0, 0, 1})
+	})
+}
+
+func TestFillingUnclosedPath1(t *testing.T) {
+	// https://github.com/LaurenzV/cpu-sparse-experiments/issues/12
+	renderAndCompare(t, 100, 100, false, "filling_unclosed_path_1", func(ctx *CsRenderCtx) {
+		var path curve.BezPath
+		path.MoveTo(curve.Pt(75, 25))
+		path.LineTo(curve.Pt(25, 25))
+		path.LineTo(curve.Pt(25, 75))
+		ctx.Fill(path.Elements(), NonZero, Color{0, 1, 0, 1})
+	})
+}
+
+func TestFillingUnclosedPath2(t *testing.T) {
+	// https://github.com/LaurenzV/cpu-sparse-experiments/issues/12
+	renderAndCompare(t, 100, 100, false, "filling_unclosed_path_2", func(ctx *CsRenderCtx) {
+		var path curve.BezPath
+		path.MoveTo(curve.Pt(50, 0))
+		path.LineTo(curve.Pt(0, 0))
+		path.LineTo(curve.Pt(0, 50))
+
+		path.MoveTo(curve.Pt(50, 50))
+		path.LineTo(curve.Pt(100, 50))
+		path.LineTo(curve.Pt(100, 100))
+		path.LineTo(curve.Pt(50, 100))
+		path.ClosePath()
+
+		ctx.Fill(path.Elements(), NonZero, Color{0, 1, 0, 1})
+	})
+}
+
+func TestTriangleExceedingViewport1(t *testing.T) {
+	// https://github.com/LaurenzV/cpu-sparse-experiments/issues/28
+	renderAndCompare(t, 15, 8, false, "triangle_exceeding_viewport_1", func(ctx *CsRenderCtx) {
+		var path curve.BezPath
+		path.MoveTo(curve.Pt(5, 0))
+		path.LineTo(curve.Pt(12, 7.99))
+		path.LineTo(curve.Pt(-4, 7.99))
+		path.ClosePath()
+
+		ctx.Fill(path.Elements(), EvenOdd, Color{0, 1, 0, 1})
+	})
+}
+
+func TestTriangleExceedingViewport2(t *testing.T) {
+	// https://github.com/LaurenzV/cpu-sparse-experiments/issues/28
+	renderAndCompare(t, 15, 8, false, "triangle_exceeding_viewport_2", func(ctx *CsRenderCtx) {
+		var path curve.BezPath
+		path.MoveTo(curve.Pt(4, 0))
+		path.LineTo(curve.Pt(11, 7.99))
+		path.LineTo(curve.Pt(-5, 7.99))
+		path.ClosePath()
+
+		ctx.Fill(path.Elements(), EvenOdd, Color{0, 1, 0, 1})
+	})
+}
+
+func TestShapeAtWideTileBoundary(t *testing.T) {
+	// https://github.com/LaurenzV/cpu-sparse-experiments/issues/30
+	ctx := getCtx(256, 4, false)
+	var path curve.BezPath
+	path.MoveTo(curve.Pt(248, 0))
+	path.LineTo(curve.Pt(257, 0))
+	path.LineTo(curve.Pt(257, 2))
+	path.LineTo(curve.Pt(248, 2))
+	path.ClosePath()
+
+	ctx.Fill(path.Elements(), EvenOdd, Color{0, 1, 0, 1})
+	// Make sure we don't panic.
+	render(ctx)
+}
+
+func TestFullCover1(t *testing.T) {
+	renderAndCompare(t, 8, 8, true, "full_cover_1", func(ctx *CsRenderCtx) {
+		c := curve.NewRectFromOrigin(curve.Pt(0, 0), curve.Sz(8, 8)).PathElements(0.1)
+		ctx.Fill(c, NonZero, Color{0.96, 0.96, 0.86, 1})
+	})
+}
+
+func TestFilledTriangle(t *testing.T) {
+	renderAndCompare(t, 100, 100, false, "filled_triangle", func(ctx *CsRenderCtx) {
+		var path curve.BezPath
+		path.MoveTo(curve.Pt(5, 5))
+		path.LineTo(curve.Pt(95, 50))
+		path.LineTo(curve.Pt(5, 95))
+		path.ClosePath()
+
+		ctx.Fill(path.Elements(), NonZero, Color{0, 1, 0, 1})
+	})
+}
+
+func TestStrokedTriangle(t *testing.T) {
+	renderAndCompare(t, 100, 100, false, "stroked_triangle", func(ctx *CsRenderCtx) {
+		var path curve.BezPath
+		path.MoveTo(curve.Pt(5, 5))
+		path.LineTo(curve.Pt(95, 50))
+		path.LineTo(curve.Pt(5, 95))
+		path.ClosePath()
+
+		ctx.Stroke(path.Elements(), curve.DefaultStroke.WithWidth(3), Color{0, 1, 0, 1})
+	})
+}
+
+func TestFilledCircle(t *testing.T) {
+	renderAndCompare(t, 100, 100, false, "filled_circle", func(ctx *CsRenderCtx) {
+		c := curve.Circle{
+			Center: curve.Pt(50, 50),
+			Radius: 45,
+		}.PathElements(0.1)
+		ctx.Fill(c, NonZero, Color{0, 1, 0, 1})
+	})
+}
+
+func TestFilledCircleWithOpacity(t *testing.T) {
+	renderAndCompare(t, 100, 100, false, "filled_circle_with_opacity", func(ctx *CsRenderCtx) {
+		c := curve.Circle{
+			Center: curve.Pt(50, 50),
+			Radius: 45,
+		}.PathElements(0.1)
+		ctx.Fill(c, NonZero, Color{0.2, 0.1, 0.3, 0.5})
+	})
+}
+
+func TestFilledOverlappingCircles(t *testing.T) {
+	renderAndCompare(t, 100, 100, false, "filled_overlapping_circles", func(ctx *CsRenderCtx) {
+		for _, e := range []struct {
+			x     float64
+			y     float64
+			color Color
+		}{
+			{35, 35, Color{0.5, 0, 0, 0.5}},
+			{65, 35, Color{0, 0.25, 0, 0.5}},
+			{50, 65, Color{0, 0, 0.5, 0.5}},
+		} {
+			circle := curve.Circle{Center: curve.Pt(e.x, e.y), Radius: 30}
+			ctx.Fill(circle.PathElements(0.1), NonZero, e.color)
+		}
+	})
+}
+
+func TestStrokedCircle(t *testing.T) {
+	renderAndCompare(t, 100, 100, false, "stroked_circle", func(ctx *CsRenderCtx) {
+		circle := curve.Circle{Center: curve.Pt(50, 50), Radius: 45}
+		stroke := curve.DefaultStroke.WithWidth(3)
+
+		ctx.Stroke(circle.PathElements(0.1), stroke, Color{0, 1, 0, 1})
+	})
+}
+
+func TestTriangleAboveAndWiderThanViewport(t *testing.T) {
+	// Requires winding of the first row of tiles to be calculcated correctly for sloped lines.
+	renderAndCompare(t, 10, 10, false, "triangle_above_and_wider_than_viewport", func(ctx *CsRenderCtx) {
+		var path curve.BezPath
+		path.MoveTo(curve.Pt(5, -5))
+		path.LineTo(curve.Pt(14, 6))
+		path.LineTo(curve.Pt(-8, 6))
+		path.ClosePath()
+
+		ctx.Fill(path.Elements(), NonZero, Color{0.2, 0.1, 0.3, 0.5})
+	})
+}
+
+func TestRectangleLeftOfViewport(t *testing.T) {
+	// Requires winding and pixel coverage to be calculcated correctly for tiles preceding the
+	// viewport in scan direction.
+	renderAndCompare(t, 10, 10, false, "rectangle_left_of_viewport", func(ctx *CsRenderCtx) {
+		rect := curve.NewRectFromPoints(curve.Pt(-4, 3), curve.Pt(1, 8))
+		ctx.Fill(rect.PathElements(0.1), NonZero, Color{0.2, 0.1, 0.3, 0.5})
+	})
+}
+
+func TestFilledAlignedRect(t *testing.T) {
+	renderAndCompare(t, 30, 20, false, "filled_aligned_rect", func(ctx *CsRenderCtx) {
+		rect := curve.NewRectFromPoints(curve.Pt(1, 1), curve.Pt(29, 19))
+		ctx.Fill(rect.PathElements(0.1), NonZero, Color{0.2, 0.1, 0.3, 0.5})
+	})
+}
+
+func TestStrokedUnalignedRect(t *testing.T) {
+	renderAndCompare(t, 30, 30, false, "stroked_unaligned_rect", func(ctx *CsRenderCtx) {
+		rect := curve.NewRectFromPoints(curve.Pt(5, 5), curve.Pt(25, 25))
+		stroke := curve.DefaultStroke.WithWidth(1).WithJoin(curve.MiterJoin)
+		ctx.Stroke(rect.PathElements(0.1), stroke, Color{0.2, 0.1, 0.3, 0.5})
+	})
+}
+
+func TestClipping(t *testing.T) {
+	renderAndCompare(t, 64, 64, true, "clipping", func(ctx *CsRenderCtx) {
+		var triangle curve.BezPath
+		triangle.MoveTo(curve.Pt(2.0, 2.0))
+		triangle.LineTo(curve.Pt(36.0, 4.0))
+		triangle.LineTo(curve.Pt(6.0, 36.0))
+		triangle.ClosePath()
+
+		circle := curve.Circle{
+			Center: curve.Pt(20.0, 20.0),
+			Radius: 19.0,
+		}
+
+		ctx.Clip(circle.PathElements(0.1), NonZero)
+		ctx.Clip(triangle.Elements(), NonZero)
+		ctx.Fill(triangle.Elements(), NonZero, [4]float32{0.5, 0.5, 0.5, 0.5})
+		ctx.Stroke(circle.PathElements(0.1), curve.DefaultStroke.WithWidth(5), [4]float32{0, 0, 1, 1})
+		ctx.Stroke(triangle.Elements(), curve.DefaultStroke.WithWidth(5), [4]float32{0, 0, 1, 1})
+	})
+}
+
+func writeF32AsU16(in []Color, out [][8]uint8) {
+	_ = out[len(in)]
+	for i := range in {
+		px := in[i]
+
+		binary.BigEndian.PutUint16(out[i][0:], uint16((px[0]/px[3])*65535+0.5))
+		binary.BigEndian.PutUint16(out[i][2:], uint16((px[1]/px[3])*65535+0.5))
+		binary.BigEndian.PutUint16(out[i][4:], uint16((px[2]/px[3])*65535+0.5))
+		binary.BigEndian.PutUint16(out[i][6:], uint16(px[3]*65535+0.5))
+	}
+}
 
 func benchmarkFill(b *testing.B, fn func(b *testing.B, buf [][stripHeight]Color)) {
 	buf := make([][stripHeight]Color, wideTileWidth)
